e013c6a30c8baa6c7a4b3c2b907cf251a6674440
Eleanor Smith <esmith88@sci5.jasmin.ac.uk>
2021-04-22 15:43:55 +0100

    Add tests and use test data

--- tests/test_xarray_pickler.py
+++ tests/test_xarray_pickler.py
@@ -3,52 +3,58 @@
 """Tests for `xarray_pickler` package."""
 
 __author__ = """Elle Smith"""
-__contact__ = 'eleanor.smith@stfc.ac.uk'
+__contact__ = "eleanor.smith@stfc.ac.uk"
 __copyright__ = "Copyright 2020 United Kingdom Research and Innovation"
 __license__ = "BSD - see LICENSE file in top-level package directory"
 
-import pytest
 import xarray as xr
 
+from xarray_pickler import CONFIG
 from xarray_pickler.xarray_pickler import _get_pickle_path, open_dset
 
-from xarray_pickler import CONFIG
+from .conftest import MINI_ESGF_CACHE_DIR
 
-def test_get_pickle_path():
-    dpath = "/badc/cmip6/data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical/r1i1p1f1/Amon/rlds/gr/v20180803"
+
+def test_get_pickle_path(tmpdir):
+    CONFIG["paths"]["cache_base_dir"] = f"{tmpdir}/fakepath"
+    dpath = "/badc/cmip6/data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical"
+    "/r1i1p1f1/Amon/rlds/gr/v20180803"
 
     pickle_path = _get_pickle_path(dpath)
 
-    assert pickle_path == "/badc/cmip6/data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical/r1i1p1f1/Amon.rlds.gr.v20180803.pickle"
+    assert (
+        pickle_path == f"{tmpdir}/fakepath/badc/cmip6/data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR"
+        "/historical/r1i1p1f1/Amon.rlds.gr.v20180803.pickle"
+    )
 
 
-def test_open_dset_default_kwargs(tmpdir):
-    CONFIG['paths']['cache_base_dir'] = tmpdir
-    dpath = "/badc/cmip6/data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical/r1i1p1f1/Amon/rlds/gr/v20180803"
+def test_open_dset_default_kwargs(tmpdir, load_test_data):
+    CONFIG["paths"]["cache_base_dir"] = tmpdir
+    dpath = f"{MINI_ESGF_CACHE_DIR}/master/test_data/badc/cmip6/data/CMIP6"
+    "/CMIP/INM/INM-CM5-0/historical/r1i1p1f1/Amon/rlds/gr1/v20190610/"
 
-    ds_original = xr.open_mfdataset(dpath, usecftime=True, combine="by_coords")
-
-    print(ds_original)
+    ds_original = xr.open_mfdataset(
+        f"{dpath}/*nc", use_cftime=True, combine="by_coords"
+    )
 
     ds = open_dset(dpath)
 
-    print(ds)
-
     assert ds == ds_original
 
-    # try and open again now that it has been pickled
+    #  try and open again now that it has been pickled
     ds_from_pickle = open_dset(dpath)
 
     assert ds_from_pickle == ds_original
 
-    print(ds_from_pickle)
 
+def test_open_dset_force_repickle(tmpdir, load_test_data):
+    CONFIG["paths"]["cache_base_dir"] = tmpdir
+    dpath = f"{MINI_ESGF_CACHE_DIR}/master/test_data/badc/cmip6/data/CMIP6/"
+    "CMIP/INM/INM-CM5-0/historical/r1i1p1f1/Amon/rlds/gr1/v20190610/"
 
-def test_open_dset_force_repickle():
-    CONFIG['paths']['cache_base_dir'] = tmpdir
-    dpath = "/badc/cmip6/data/CMIP6/CMIP/IPSL/IPSL-CM6A-LR/historical/r1i1p1f1/Amon/rlds/gr/v20180803"
-
-    ds_original = xr.open_mfdataset(dpath, usecftime=True, combine="by_coords")
+    ds_original = xr.open_mfdataset(
+        f"{dpath}/*nc", use_cftime=True, combine="by_coords"
+    )
 
     ds_pickle = open_dset(dpath, force_repickle=True)
 
--- xarray_pickler/xarray_pickler.py
+++ xarray_pickler/xarray_pickler.py
@@ -1,36 +1,37 @@
 """Main module."""
 
 __author__ = """Elle Smith"""
-__contact__ = 'eleanor.smith@stfc.ac.uk'
+__contact__ = "eleanor.smith@stfc.ac.uk"
 __copyright__ = "Copyright 2020 United Kingdom Research and Innovation"
 __license__ = "BSD - see LICENSE file in top-level package directory"
 
-import glob
 import os
-import xarray as xr
 import pickle
+
+import xarray as xr
 
 from xarray_pickler import CONFIG, logging
 
 logger = logging.getLogger(__file__)
+
 
 def _get_pickle_path(dpath):
     """
     Define a "grouped" path that splits facets across directories and then groups
     the final set into a file path, based on dir_grouping_level value in CONFIG
     """
-    cache = CONFIG['paths']['cache_base_dir']
-    gl = CONFIG['paths']['dir_grouping_level']
+    cache = CONFIG["paths"]["cache_base_dir"]
+    gl = CONFIG["paths"]["dir_grouping_level"]
 
-    if not os.path.isdir(cache):
-        os.makedirs(cache)
-
-    parts = dpath.split("/")
+    parts = dpath.split("/")[1:]
     grouped_path = "/".join(parts[:-gl]) + "/" + ".".join(parts[-gl:])
 
-    pickle_path = os.path.join(cache, grouped_path + ".pickle")
+    pickle_path = os.path.join(cache, grouped_path)
 
-    return pickle_path
+    if not os.path.isdir(pickle_path):
+        os.makedirs(pickle_path)
+
+    return pickle_path + ".pickle"
 
 
 def open_dset(dpath, force_repickle=False, **kwargs):
@@ -46,15 +47,16 @@ def open_dset(dpath, force_repickle=False, **kwargs):
     if os.path.isfile(pickle_path) and not force_repickle:
         try:
             with open(pickle_path, "rb") as reader:
-               return pickle.load(reader)
+                return pickle.load(reader)
         except Exception:
             # Assume failure so try to re-read and re-pickle the file
             pass
 
     ds = xr.open_mfdataset(fpattn, **open_kwargs)
+    ds.close()
 
     logger.info(f"Pickling dataset to: {pickle_path}")
-    with open(pickle_path, 'wb') as writer:
-        pkl = pickle.dump(ds, writer, protocol=-1)
+    with open(pickle_path, "wb") as writer:
+        pickle.dump(ds, writer, protocol=-1)
 
     return ds
